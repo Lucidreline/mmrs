8d23b92f2281dbefee3ed5bb92104cc9c29bb0e4
Lucidreline <casta.ma502@gmail.com>
2021-01-27 00:28:33 -0800

    The map now works when the app is built. Allowed CSP for images.

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,23 @@
+import helmet from 'helmet'
+import express from 'express'
+import path from 'path'
 
+const app = express()
+
+app.use(helmet())
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+// serve static assets in front end
+app.use(express.static('../client/build'))
+
+app.get('*', (req, res) => {
+  res.setHeader('Content-Security-Policy', 'img-src https: data:;')
+  res.sendFile(
+    path.resolve(__dirname, '../', '../', 'client', 'build', 'index.html'),
+  )
+})
+
+app.listen(1800, () => console.log('Server Online!'))
