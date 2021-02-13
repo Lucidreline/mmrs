72bca72ce6a2039b02559bc4720822ae8668a6f6
Lucidreline <techservs@pm.me>
2021-02-13 01:45:02 -0800

    Fixed the adventure page.

--- client/src/components/pages/adventure-page/adventure-page.component.tsx
+++ client/src/components/pages/adventure-page/adventure-page.component.tsx
@@ -1,23 +1,36 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
+import ThumbnailList from '../../thumbnail-list/thumbnail-list.component'
+
+import './adventurePage.styles.scss'
 
 interface IAdventure {
   _id: string
-  name: number
+  name: string
   description: string
   date: string
   pictures: string[]
 }
 
+
+
 const AdventurePage = () => {
-  const [Adventure, setAdventure] = useState<IAdventure>()
+  const [Adventure, setAdventure] = useState<IAdventure>({
+    name: 'hi',
+    pictures: [''],
+    date: '',
+    description: '',
+    _id: ''
+  })
+
+  
 
   const urlArr = useLocation().pathname.split('/')
   const adventureId = urlArr[urlArr.length - 1]
-
+ 
   useEffect(() => {
-    async function fetchlocation() {
+    async function fetchAdventure() {
       const responce = await axios.get(
         `${process.env.REACT_APP_API_ORIGIN}/api/adventures/id`,
         {
@@ -26,17 +39,26 @@ const AdventurePage = () => {
           },
         },
       )
-      setAdventure(responce.data)
+      setAdventure(await responce.data.adventure)
     }
-    fetchlocation()
+    fetchAdventure()
   }, [adventureId])
-  return (
-    <div className="container">
-      <h2 className="page-title">Adventure Name</h2>
-      <p>{Adventure?.date}</p>
-      {/* <ThumbnailList imageUrls={Adventure.pictures} /> */}
+
+const formattedDate = (): string => {
+    const adventureDate = new Date(parseInt(Adventure.date)).toString()
+    const trimmedString = adventureDate.split(' ').slice(1, 4).join(' ')
+    return trimmedString
+  }
+
+ return(
+  <div className="container adventure-page">
+      <h2 className="page-title">{Adventure.name}</h2>
+      <p className='description'>{Adventure.description}</p>
+      <span className='date'>{formattedDate()}</span>
+      
+      <ThumbnailList imageUrls={Adventure.pictures}/>
     </div>
-  )
+ )
 }
 
 export default AdventurePage
