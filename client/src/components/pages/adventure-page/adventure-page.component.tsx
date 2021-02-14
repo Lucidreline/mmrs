c89c1fed99b66cfc3edec0bc025817cfe50ab0d7
Lucidreline <casta.ma502@gmail.com>
2021-02-14 00:19:15 -0800

    Adventure page now has access to its Location. Location link in Adventure Page.

--- client/src/components/pages/adventure-page/adventure-page.component.tsx
+++ client/src/components/pages/adventure-page/adventure-page.component.tsx
@@ -1,29 +1,40 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { formatDateFromString } from '../../../utils/functions'
-import { IAdventure } from '../../../utils/types'
+import { IAdventure, ILocation } from '../../../utils/types'
 import ThumbnailList from '../../thumbnail-list/thumbnail-list.component'
 
 import './adventurePage.styles.scss'
 
+interface IResponce {
+  data: {
+    adventure: IAdventure
+    location: ILocation
+  }
+  [key: string]: any
+}
+
 const AdventurePage = () => {
   const [Adventure, setAdventure] = useState<IAdventure>()
+  const [Location, setLocation] = useState<ILocation>()
 
   const urlArr = useLocation().pathname.split('/')
   const adventureId = urlArr[urlArr.length - 1]
 
   useEffect(() => {
     async function fetchAdventure() {
-      const responce = await axios.get(
+      const responce: IResponce = await axios.get(
         `${process.env.REACT_APP_API_ORIGIN}/api/adventures/id`,
         {
           params: {
             adventureId,
+            includeLocation: true,
           },
         },
       )
-      setAdventure(await responce.data.adventure)
+      setAdventure(responce.data.adventure)
+      setLocation(responce.data.location)
     }
     fetchAdventure()
   }, [adventureId])
@@ -32,6 +43,9 @@ const AdventurePage = () => {
     <div className="container adventure-page">
       <h2 className="page-title">{Adventure?.name}</h2>
       <p className="description">{Adventure?.description}</p>
+      <Link to={`/location/${Location?._id}`}>
+        <span className="location-link">{Location?.name}</span>
+      </Link>
       <span className="date">{formatDateFromString(Adventure?.date)}</span>
 
       <ThumbnailList
