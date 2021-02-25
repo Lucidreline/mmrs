58c94d26dbfefd4e234832f450dbc625601a1557
Lucidreline <casta.ma502@gmail.com>
2021-02-24 22:08:56 -0800

    Created a method to delete multiple images from cloudinary using their urls.

--- server/utils/cloudinary.ts
+++ server/utils/cloudinary.ts
@@ -9,4 +9,30 @@ Cloudinary.config({
   api_secret: config.get('cloudinarySecret'),
 })
 
+// code from my bobby :)
+const getPublicID = (url: string) => {
+  const urlArray = url.split('/')
+
+  for (let i = 0; i < 7; i++) urlArray.shift()
+
+  let publicID = ''
+  for (let i = 0; i < urlArray.length; i++) {
+    publicID += urlArray[i] + '/'
+  }
+
+  const lastIndex = publicID.lastIndexOf('.')
+  publicID = publicID.substring(0, lastIndex)
+  return publicID
+}
+
+export const deleteImagesFromCloudinary = async (images: string[]) => {
+  let publicIDs: string[] = []
+
+  for (let i = 0; i < images.length; i++) {
+    publicIDs.push(getPublicID(images[i]))
+  }
+
+  await Cloudinary.api.delete_resources(publicIDs)
+}
+
 export default Cloudinary
